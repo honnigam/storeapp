13a520ee1e507f8b39e3d0f30adf19e9ec0f1a16
honnigam <lucasvm87@gmail.com>
2023-10-25 20:19:21 -0300

    feat: create a sidebar

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,48 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const header  = () => {
   return ( 
     <Card className="flex justify-between p-[1.875rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon/>
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon/>
+          </Button>
+        </SheetTrigger>
+        
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold"> 
+          Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-4">
+
+            <Button className="w-full justify-start gap-2" variant="outline"> 
+              <LogInIcon size={16}/>
+               Fazer Login
+            </Button>
+
+            <Button  className="w-full justify-start gap-2" variant="outline"> 
+              <HomeIcon size={16}/>
+               Início
+            </Button>
+
+            <Button  className="w-full justify-start gap-2" variant="outline"> 
+              <PercentIcon size={16}/>
+               Ofertas
+            </Button>
+
+            <Button  className="w-full justify-start gap-2" variant="outline"> 
+              <ListOrderedIcon size={16}/>
+               Catálogo
+            </Button>
+
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="font-semibold text-lg">Store 
       <span className="text-primary"> APP</span>
